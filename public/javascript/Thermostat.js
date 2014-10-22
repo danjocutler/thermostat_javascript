9dd52c652187d9aebb9b45d24b306e156869d902
danjocutler <danjocutler@gmail.com>
2014-10-22 18:59:02 +0100

    Added some html, sinatra and jquery functions

--- public/javascript/Thermostat.js
+++ public/javascript/Thermostat.js
@@ -7,23 +7,33 @@ function Thermostat(){
 Thermostat.prototype.increaseTemperature = function() {
 	this.increaseTemperatureBy(1);
 };
+
 Thermostat.prototype.decreaseTemperature = function() {
 	this.decreaseTemperatureBy(1);
 };
+
 Thermostat.prototype.increaseTemperatureBy = function(degrees) {
 	this.temperature += degrees
 	if(this.temperature > 25 && this.isPowerSaverOn === true) return new Error("Power saving mode is ON. Cannot exceed 25 degrees");
 	if(this.temperature > 32 && this.isPowerSaverOn === false) return new Error("Cannot exceed 32 degrees");
-
 };
+
 Thermostat.prototype.decreaseTemperatureBy = function(degrees) {
 	this.temperature -= degrees;
-	if(this.temperature < 10) return new Error("Cannot exceed minimum temperature of 10");
+	
 };
 
 Thermostat.prototype.powerSaverButton = function(onOff) {
 	if (onOff === "off") return this.isPowerSaverOn = false;
 	if (onOff === "on") return this.isPowerSaverOn;
+};
+
+Thermostat.prototype.resetToDefaultTemperature = function() {
+	return this.temperature = 20
+};
+
+Thermostat.prototype.errorMessage = function() {
+	return this.temperature = 20
 };
 
 // Thermostat.prototype.minimumTemperature = function() {
--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -20,6 +20,10 @@ describe ('Thermostat', function(){
 			expect(thermostat.isPowerSaverOn).toBe(true);
 		});
 
+	});
+
+	describe('custom options', function(){
+
 		it('can increase the temperature by one degree', function() {
 			thermostat.increaseTemperature();
 			expect(thermostat.temperature).toEqual(21);
@@ -30,28 +34,12 @@ describe ('Thermostat', function(){
 			expect(thermostat.temperature).toEqual(19);
 		});		
 
-		it('cannot go below minimum temperature', function() {
-			expect(thermostat.decreaseTemperatureBy(11)).toEqual(new Error("Cannot exceed minimum temperature of 10"));
-		});
-
-		it('with powersaver on, temp cannot exceed 25 degrees', function() {
-			expect(thermostat.increaseTemperatureBy(6)).toEqual(new Error("Power saving mode is ON. Cannot exceed 25 degrees"));
-		});
-
-		it('with powersaver off, temp cannot exceed 32 degrees', function() {
-			thermostat.powerSaverButton("off");
-			expect(thermostat.increaseTemperatureBy(13)).toEqual(new Error("Cannot exceed 32 degrees"));
-		});		
-	});
-
-	describe('custom options', function(){
-
-		it('can increase the temperature by 10', function(){
+		it('can increase the temperature by 10 degrees', function(){
 			thermostat.increaseTemperatureBy(10);
 			expect(thermostat.temperature).toEqual(30);
 		});
 
-		it('can decrease the temperature by 10', function(){
+		it('can decrease the temperature by 10 degrees', function(){
 			thermostat.decreaseTemperatureBy(10);
 			expect(thermostat.temperature).toEqual(10);
 		});
@@ -65,6 +53,28 @@ describe ('Thermostat', function(){
 			thermostat.powerSaverButton("on");
 			expect(thermostat.isPowerSaverOn).toBe(true);
 		});
+
+		it('should be able to reset to the default temperature', function(){
+			thermostat.increaseTemperatureBy(10);
+			thermostat.resetToDefaultTemperature();
+			expect(thermostat.temperature).toEqual(20);
+		});
+	});
+
+	describe('warnings', function(){
+		
+		// it('cannot go below minimum temperature', function() {
+		// 	expect(thermostat.decreaseTemperatureBy(11)).toEqual(errorMessage);   //(new Error("Cannot exceed minimum temperature of 10"));
+		// });
+
+		it('with powersaver on, temp cannot exceed 25 degrees', function() {
+			expect(thermostat.increaseTemperatureBy(6)).toEqual(new Error("Power saving mode is ON. Cannot exceed 25 degrees"));
+		});
+
+		it('with powersaver off, temp cannot exceed 32 degrees', function() {
+			thermostat.powerSaverButton("off");
+			expect(thermostat.increaseTemperatureBy(13)).toEqual(new Error("Cannot exceed 32 degrees"));
+		});	
 
 	});
 });
