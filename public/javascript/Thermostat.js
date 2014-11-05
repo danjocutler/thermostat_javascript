1f77ebf7160d3a7516ef71961958fc51b70e78bc
danjocutler <danjocutler@gmail.com>
2014-11-05 15:10:22 +0000

    Added jquery functionality. Can increase temp to the limit.

--- public/javascript/Thermostat.js
+++ public/javascript/Thermostat.js
@@ -15,31 +15,25 @@ Thermostat.prototype.decreaseTemperature = function() {
 };
 
 Thermostat.prototype.increaseTemperatureBy = function(degrees) {
-	this.temperature += degrees;
-	if(this.temperature > this.saverMaxTemp && this.isPowerSaverOn === true) return this.saverMaxTemp;
-	else if(this.temperature > this.maxTemp && this.isPowerSaverOn === false) return this.maxTemp;
-	else return this.temperature;
+	return this.temperature += degrees;
 };
 
 Thermostat.prototype.decreaseTemperatureBy = function(degrees) {
-	this.temperature -= degrees;
-	if(this.temperature < this.minTemp) return this.minTemp;
-	else return this.temperature;	
+	return this.temperature -= degrees;
 };
 
 Thermostat.prototype.powerSaverButton = function(onOff) {
 	if (onOff === "off") return this.isPowerSaverOn = false;
-	if (onOff === "on") return this.isPowerSaverOn;
+	else return this.isPowerSaverOn;
 };
 
 Thermostat.prototype.resetToDefaultTemperature = function() {
 	return this.temperature = 20
 };
 
-Thermostat.prototype.errorMessage = function() {
-	return this.temperature = 20
-};
+	// if(this.temperature > this.saverMaxTemp && this.isPowerSaverOn === true) return this.saverMaxTemp;
+	// else if(this.temperature > this.maxTemp && this.isPowerSaverOn === false) return this.maxTemp;
+	// else return this.temperature;
 
-// Thermostat.prototype.minimumTemperature = function() {
-// 	if this.decreaseTemperature >
-// };
+	// if(this.temperature < this.minTemp) return this.minTemp;
+	// else return this.temperature;	
